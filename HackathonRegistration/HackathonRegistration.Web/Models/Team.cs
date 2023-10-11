eab32000aab0e832716cf9c21944b89edc1c3221
Jeff Fattic <jfattic@microsoft.com>
2023-10-11 08:18:11 -0500

    initial project created

---
+++ HackathonRegistration/HackathonRegistration.Web/Models/Team.cs
@@ -0,0 +1,20 @@
+﻿namespace HackathonRegistration.Web.Models
+{
+    public class Team
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string TeamName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Possible states are: US Central, Western Europe, East Asia
+        /// </summary>
+        public string EventTimezone { get; set; } = string.Empty;
+
+        public string UseCase { get; set; } = string.Empty;
+
+        public string SubscriptionId { get; set; } = string.Empty;
+
+        public List<TeamMember> TeamMembers { get; } = new List<TeamMember>();
+    }
+}
---
+++ HackathonRegistration/HackathonRegistration.Web/Models/TeamMember.cs
@@ -0,0 +1,16 @@
+﻿namespace HackathonRegistration.Web.Models
+{
+    public class TeamMember
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string EmailAddress { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Possible states are: Invited, Confirmed, Declined
+        /// </summary>
+        public string State { get; set; } = string.Empty;
+
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+    }
+}
